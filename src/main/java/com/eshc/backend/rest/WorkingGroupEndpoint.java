8ce1979eedfaafd3640f5d12faea10d40b10f489
danielmcc123 <danielmcc123123@gmail.com>
2018-10-10 10:20:01 +0100

    Add keycloak to spring boot
    Security is now configurable from application.properties file

--- src/main/java/com/eshc/backend/rest/WorkingGroupEndpoint.java
+++ src/main/java/com/eshc/backend/rest/WorkingGroupEndpoint.java
@@ -18,7 +18,7 @@ import java.util.NoSuchElementException;
 
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 
-@CrossOrigin
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("api/working")
 @Produces(APPLICATION_JSON)
