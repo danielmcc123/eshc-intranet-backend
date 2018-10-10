71620b148d4cf5a60b46df39dbfd5d3c1b22499f
danielmcc123 <danielmcc123123@gmail.com>
2018-10-10 10:20:01 +0100

    Add keycloak to spring boot
    Security is now configurable from application.properties file

--- src/main/java/com/eshc/backend/EshcBackendApplication.java
+++ src/main/java/com/eshc/backend/EshcBackendApplication.java
@@ -1,9 +1,14 @@
 package com.eshc.backend;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(exclude = org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class)
+
+
+
+//@SpringBootApplication(exclude = org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class)
+@SpringBootApplication
 public class EshcBackendApplication {
 
 	public static void main(String[] args) {
